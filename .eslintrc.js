094f9747be9e5bfbcb5a9b00f394054a9dd03b11
Mahdi A <sahandprojects@gmail.com>
2024-01-21 01:33:58 -0800

    eslint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,10 +27,7 @@ module.exports = {
       'error',
       2
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
+    'linebreak-style': 'off',
     'quotes': [
       'error',
       'single'
