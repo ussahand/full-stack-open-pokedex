e6a60918c833684ba89d92cfa3ee58852e0e8b04
ussahand <hid.mail1@gmail.com>
2024-03-28 16:11:12 -0700

    disable console.log in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ const App = () => {
   const match = useMatch('/pokemon/:name')
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
-  console.log('direcorty is ==>:) ',__dirname); 
-
+  // console.log('direcorty is ==>:) ',__dirname)
 
   if (isLoading) {
     return <LoadingSpinner />
